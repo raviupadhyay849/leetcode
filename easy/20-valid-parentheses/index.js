fa5e466f9034bab3d34812b85113d28b7495f4c4
ravi upadhyay <raviupadhyay849@gmail.com>
2025-11-22 12:17:03 +0530

    valid parentheses

--- easy/20-valid-parentheses/index.js
+++ easy/20-valid-parentheses/index.js
@@ -2,3 +2,28 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/valid-parentheses/
 
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const brackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    const stack = [];
+    
+    for (i = 0; i < s.length; i++) {
+        const lastStackElement = stack[stack.length - 1];
+        
+        if (brackets[lastStackElement] === s[i]) {
+            stack.pop();
+        } else if (brackets[s[i]]) {
+            stack.push(s[i]);
+        } else {
+            return false;
+        }
+    }
+    return !stack.length;
+};  
