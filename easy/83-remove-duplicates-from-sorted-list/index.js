241ec777d56d41f36bd63d04a0aba07d6d5876e5
ravi upadhyay <raviupadhyay849@gmail.com>
2025-11-24 13:56:23 +0530

    remove duplicate from sorted list

--- easy/83-remove-duplicates-from-sorted-list/index.js
+++ easy/83-remove-duplicates-from-sorted-list/index.js
@@ -2,3 +2,17 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let index = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[index] = nums[i];
+            index++;
+        }
+    }
+    return index;
+};
