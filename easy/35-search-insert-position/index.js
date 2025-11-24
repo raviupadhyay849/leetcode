c8118b9284dc47385ea4028b69235929e7a51ba0
ravi upadhyay <raviupadhyay849@gmail.com>
2025-11-24 13:59:10 +0530

    search insert position

--- easy/35-search-insert-position/index.js
+++ easy/35-search-insert-position/index.js
@@ -2,3 +2,19 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/search-insert-position/
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    var position = 0;
+    for(let i=0;i<nums.length;i++) {
+        if(nums[i]===target) {
+            return i;
+        } else if(target>nums[i]) {
+            position++;
+        }
+    }
+    return position;
+};
