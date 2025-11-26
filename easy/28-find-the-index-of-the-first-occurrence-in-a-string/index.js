dd93d095a32c1bac3ed7cf54c4cbb2c8af428e77
ravi upadhyay <raviupadhyay849@gmail.com>
2025-11-26 22:13:55 +0530

    find-the-index-of-the-first-occurr

--- easy/28-find-the-index-of-the-first-occurrence-in-a-string/index.js
+++ easy/28-find-the-index-of-the-first-occurrence-in-a-string/index.js
@@ -2,3 +2,27 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  let i = 0;
+  let find = false ;
+  while(i<haystack.length && find == false){
+       const string = haystack.slice(i,i+needle.length);
+       if(string === needle){
+           find = true ;
+       }
+       else{
+           i++;
+       }     
+  }
+  if(find == true){
+      return i ; 
+  }
+  else{
+      return -1 ;
+  }  
+};
