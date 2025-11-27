4b9cc941e7aa6c12598149581ce9c54ff5c2d029
ravi upadhyay <raviupadhyay849@gmail.com>
2025-11-27 17:27:17 +0530

    plus-one

--- easy/66-plus-one/index.js
+++ easy/66-plus-one/index.js
@@ -2,3 +2,26 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/plus-one/
 
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+for(let i=digits.length-1; i>=0;i--)
+{
+    if(digits[i]!==9)
+    {
+        digits[i]=digits[i]+1;
+    }
+    else if(i===0)
+    {
+        digits[i]=0;
+        digits.unshift(1);
+    }
+    else{
+        digits[i]=0;
+        continue;
+    }
+    return digits ;
+}
+};
