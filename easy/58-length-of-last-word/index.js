59e196af5b03297454d928b8fd21253c5fe60941
ravi upadhyay <raviupadhyay849@gmail.com>
2025-11-27 17:02:35 +0530

    length-of-last-word

--- easy/58-length-of-last-word/index.js
+++ easy/58-length-of-last-word/index.js
@@ -2,3 +2,19 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/length-of-last-word/
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    for(var i=0;i<s.length;i++){
+        if(s[i]!==" ") break;
+    }
+    s=s.slice(i,s.length);
+    for(var j=s.length-1;j>=0;j--){
+        if(s[j]!==" ")break;
+    }
+    s=s.slice(0,j+1);
+    var res = s.split(" ")
+    return res[res.length-1].length;
+};
