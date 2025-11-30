8cb30a6074643c816d28e3447dc2b8da38cee198
ravi upadhyay <raviupadhyay849@gmail.com>
2025-11-30 22:50:57 +0530

    inorder traversal

--- easy/94-binary-tree-inorder-traversal/index.js
+++ easy/94-binary-tree-inorder-traversal/index.js
@@ -2,3 +2,32 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/binary-tree-inorder-traversal/
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const res = [];
+    const stack = [];
+
+    while (root || stack.length > 0) {
+        while (root) {
+            stack.push(root);
+            root = root.left;
+        }
+
+        root = stack.pop();
+        res.push(root.val);
+        root = root.right;
+    }
+
+    return res;    
+};
