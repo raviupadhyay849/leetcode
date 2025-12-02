1e4a9792547e1885530cb094bb7d32339592fac9
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-02 16:11:41 +0530

    convert sorted array to binry search

--- easy/108-convert-sorted-array-to-binary-search-tree/index.js
+++ easy/108-convert-sorted-array-to-binary-search-tree/index.js
@@ -2,3 +2,26 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums) {
+    const build = (start, end) => {
+        if (start === end) return null;
+        const mid = Math.floor((start + end) / 2);
+        const node = new TreeNode(nums[mid]);
+        node.left = build(start, mid);
+        node.right = build(mid + 1, end);
+        return node;
+    };
+    return build(0, nums.length);
+};
