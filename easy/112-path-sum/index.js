dcb04fe5a9b2774ac39d69177476f49683c472b6
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-03 19:51:34 +0530

    path sum

--- easy/112-path-sum/index.js
+++ easy/112-path-sum/index.js
@@ -2,3 +2,28 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/path-sum/
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    if (!root) return false;
+    const rec = (node, sum) => {
+        if (!node?.left && !node?.right) {
+            return sum === targetSum;
+        }
+        const left = rec(node.left, sum + node.left?.val);
+        const right = rec(node.right, sum + node.right?.val);
+        return left || right;
+    }
+    return rec(root, root.val);
+};
