bc7c06d877ece18e0b521e32c8b0e1a670f2b02d
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-04 16:53:07 +0530

    pascals-triangle-2

--- easy/119-pascals-triangle-ii/index.js
+++ easy/119-pascals-triangle-ii/index.js
@@ -2,3 +2,14 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/pascals-triangle-ii/
 
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(r) {
+    var ans = new Array(r+1)
+    ans[0]=ans[r]=1
+    for(i=1,up=r;i<r;i++,up--)
+        ans[i] = ans[i-1]*up/i
+    return ans
+};
