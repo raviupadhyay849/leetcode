13b4069c7573b7f1de09317e6d9868aea54e6280
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-04 16:57:49 +0530

    best time to buy and sell stock

--- easy/121-best-time-to-buy-and-sell-stock/index.js
+++ easy/121-best-time-to-buy-and-sell-stock/index.js
@@ -2,3 +2,17 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if(prices == null || prices.length <= 1) return 0;
+    let minBuy = prices[0];
+    let profit = 0;
+    for(let i = 1; i < prices.length; i++) {
+        minBuy = Math.min(minBuy, prices[i]);
+        profit = Math.max(profit, prices[i] - minBuy);
+    }
+    return profit;
+};
