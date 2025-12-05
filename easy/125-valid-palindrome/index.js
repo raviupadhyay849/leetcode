51759a2d8e74929ab3024f19ef58295a99f83a5c
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-05 21:25:28 +0530

    valid palindrom

--- easy/125-valid-palindrome/index.js
+++ easy/125-valid-palindrome/index.js
@@ -2,3 +2,20 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/valid-palindrome/
 
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let newStr = '';
+    for (let i of s) {
+        if (/[a-zA-Z0-9]/.test(i)) {
+            newStr += i.toLowerCase();
+        }
+    }
+    let l = 0, r = newStr.length - 1;
+    while (l < r) {
+        if (newStr[l++] !== newStr[r--]) return false;
+    }
+    return true;
+};
