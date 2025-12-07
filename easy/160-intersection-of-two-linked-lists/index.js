c112d53f426a85a08227790010ab45dc6abe7229
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-07 18:57:44 +0530

    two linked list

--- easy/160-intersection-of-two-linked-lists/index.js
+++ easy/160-intersection-of-two-linked-lists/index.js
@@ -2,3 +2,27 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/intersection-of-two-linked-lists/
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let lista = headA;
+    let listb = headB;
+
+    while (lista !== listb) {
+        lista = lista ? lista.next : headB;
+        listb = listb ? listb.next : headA;
+    }
+
+    return lista;    
+};
