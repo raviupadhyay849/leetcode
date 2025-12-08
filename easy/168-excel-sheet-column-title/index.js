9214825fa72f04fbfd8aa220853b98a81349c5a0
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-08 23:41:02 +0530

    excel colum title

--- easy/168-excel-sheet-column-title/index.js
+++ easy/168-excel-sheet-column-title/index.js
@@ -2,3 +2,16 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/excel-sheet-column-title/
 
+/**
+ * @param {number} columnNumber
+ * @return {string}
+ */
+var convertToTitle = function(columnNumber) {
+    let ans = "";
+    while(columnNumber !== 0){
+        columnNumber--;
+        ans = String.fromCharCode((columnNumber%26)+65) + ans;
+        columnNumber = Math.floor(columnNumber/26);
+    }
+    return ans;
+};
