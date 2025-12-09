85c2a9b3ec85459454e251eceb22d4ee70726158
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-09 22:27:52 +0530

    majority element

--- easy/169-majority-element/index.js
+++ easy/169-majority-element/index.js
@@ -2,3 +2,18 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/majority-element/
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let freq = new Map();
+    let n = nums.length;
+    for (let num of nums) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+        if (freq.get(num) > Math.floor(n / 2)) {
+            return num;
+        }
+    }
+    return -1;
+};
