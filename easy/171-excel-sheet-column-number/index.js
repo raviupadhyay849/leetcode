20f4dc8c4ec1c5cbbcef236a8afff11bba6a08df
ravi upadhyay <raviupadhyay849@gmail.com>
2025-12-12 13:50:28 +0530

    select column number

--- easy/171-excel-sheet-column-number/index.js
+++ easy/171-excel-sheet-column-number/index.js
@@ -2,3 +2,14 @@
  // Difficulty: EASY
  // URL: https://leetcode.com/problems/excel-sheet-column-number/
 
+/**
+ * @param {string} columnTitle
+ * @return {number}
+ */
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    for (let i = 0; i < columnTitle.length; i++) {
+        result = result * 26 + (columnTitle.charCodeAt(i) - 64);
+    }
+    return result;
+};      
